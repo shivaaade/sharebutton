909af0e54119a860b449ab1c74948e37b6446310
Aade Shiva <adheshiva02@gmail.com>
2023-03-30 12:03:33 +0530

    first commit

---
+++ sharing/src/App.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+} from "react-share";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    const shareUrl = "https://www.youtube.com/watch?v=9WzIACv_mxs";
+    return (
+      <div>
+        <FacebookShareButton url={shareUrl} quote="share link">
+          <FacebookIcon />
+        </FacebookShareButton>
+        <WhatsappShareButton url={shareUrl} quote="share link">
+          <WhatsappIcon />
+        </WhatsappShareButton>
+      </div>
+    );
+  }
+}
+
+export default App;
